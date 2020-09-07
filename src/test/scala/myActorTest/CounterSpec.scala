73690eb2296d4aaa4febe2e67a05e47d3500b54b
Kacper Korban <kacper.f.korban@gmail.com>
2020-09-07 21:23:24 +0200

    Bump Scala to 2.13.3 and add akka-typed example

--- src/test/scala/myActorTest/CounterSpec.scala
+++ src/test/scala/myActorTest/CounterSpec.scala
@@ -1,18 +1,15 @@
-
 package myActorTest
+
 import akka.testkit.TestKit
 import akka.actor.ActorSystem
-import org.scalatest.WordSpecLike
 import org.scalatest.BeforeAndAfterAll
 import akka.testkit.TestActorRef
+import org.scalatest.wordspec.AnyWordSpecLike
 
-class CounterSpec extends TestKit(ActorSystem("CounterSpec"))
-  with WordSpecLike with BeforeAndAfterAll  {
-  
-  
-  override def afterAll(): Unit = {
+class CounterSpec extends TestKit(ActorSystem("CounterSpec")) with AnyWordSpecLike with BeforeAndAfterAll {
+
+  override def afterAll(): Unit =
     system.terminate
-  }
 
   "A Counter" must {
 
@@ -20,10 +17,9 @@ class CounterSpec extends TestKit(ActorSystem("CounterSpec"))
       import Counter._
       val counter = TestActorRef[Counter]
       counter ! Incr
-      assert (counter.underlyingActor.count == 1)
-      }
-    
-   }
+      assert(counter.underlyingActor.count == 1)
+    }
+
+  }
 
 }
-
